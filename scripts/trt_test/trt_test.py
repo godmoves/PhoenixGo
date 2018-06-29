9fa19ecfea85d1dcb68cb47c1922c40b56fde6c7
godmoves <514158072@qq.com>
2018-06-29 17:54:43 +0800

    update py test code, remove unnecessary function

--- scripts/trt_test/trt_test.py
+++ scripts/trt_test/trt_test.py
@@ -1,36 +1,13 @@
 import tensorrt as trt
 import numpy as np
 import pycuda.driver as cuda
-import subprocess
 
 
-PLAN_NAME = "test.PLAN"
-UFF_TO_PLAN_EXE_PATH = '../u2p/build/uff_to_plan'
-UFF_FILENAME = '../leelaz-model-0.uff'
-
-
-def graphToPlan(uff_model_name, plan_filename, input_name, policy_name,
-                value_name, max_batch_size, max_workspace_size, data_type):
-
-    # convert frozen graph to engine (plan)
-    args = [
-        uff_model_name,
-        plan_filename,
-        input_name,
-        policy_name,
-        value_name,
-        str(max_batch_size),
-        str(max_workspace_size),
-        data_type  # float / half
-    ]
-    subprocess.call([UFF_TO_PLAN_EXE_PATH] + args)
-
+PLAN_NAME = "../leelaz-model-0.PLAN"
 
 fake_input = np.zeros(19 * 19 * 18, dtype=np.float32)
 fake_input[19 * 19 * 16: 19 * 19 * 17] = np.ones(19 * 19, dtype=np.float32)
 
-graphToPlan(UFF_FILENAME, "test.PLAN", "inputs",
-            "policy", "value", 4, 1 << 20, "float")
 policy = np.empty(362, dtype=np.float32)
 value = np.empty(1, dtype=np.float32)
 
