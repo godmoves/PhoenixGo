132dfde9511124e1a403a479359c6c2d6e742957
ManKit Pong <mankit@ManKits-MacBook-Air.local>
2019-01-30 13:01:18 +0800

    update build trt model

--- model/build_tensorrt_model.cc
+++ model/build_tensorrt_model.cc
@@ -30,11 +30,11 @@
 
 DEFINE_string(model_path, "", "Path of model.");
 DEFINE_string(data_type, "FP32", "Data type to build, FP32 or FP16.");
+DEFINE_string(input_name, "inputs", "Input name of model.");
+DEFINE_string(policy_name, "policy", "Policy head name of model");
+DEFINE_string(value_name, "value", "Value head name of model.");
 DEFINE_int32(max_batch_size, 4, "Max size for input batch.");
 DEFINE_int32(max_workspace_size, 1<<30, "Parameter to control memory allocation (in bytes).");
-DEFINE_int32(calib_iterations, 10000, "Num of iterations to run while calibration.");
-DEFINE_string(storage_address, "", "Address of wegostorage");
-DEFINE_int32(gpu, 0, "Gpu used while building.");
 
 using namespace nvinfer1;
 using namespace nvuffparser;
@@ -42,34 +42,44 @@ using namespace nvuffparser;
 class Logger : public ILogger {
   void log(Severity severity, const char *msg) override {
     switch (severity) {
-      case Severity::kINTERNAL_ERROR: LOG(ERROR) << msg; break;
-      case Severity::kERROR: LOG(ERROR) << msg; break;
-      case Severity::kWARNING: LOG(WARNING) << msg; break;
-      case Severity::kINFO: LOG(INFO) << msg; break;
+    case Severity::kINTERNAL_ERROR:
+      LOG(ERROR) << msg;
+      break;
+    case Severity::kERROR:
+      LOG(ERROR) << msg;
+      break;
+    case Severity::kWARNING:
+      LOG(WARNING) << msg;
+      break;
+    case Severity::kINFO:
+      LOG(INFO) << msg;
+      break;
     }
   }
 } g_logger;
 
 int main(int argc, char *argv[]) {
+  /* parse command line arguments */
   gflags::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
   google::InstallFailureSignalHandler();
 
   std::string uff_path = FLAGS_model_path + ".uff";
-  std::string calib_path = FLAGS_model_path + ".calib";
   std::string output_path = FLAGS_model_path + "." + FLAGS_data_type + ".PLAN";
 
+  /* parse uff */
   IUffParser *parser = createUffParser();
-  parser = createUffParser();
-  parser->registerInput("inputs", DimsCHW(19, 19, 17));
-  parser->registerOutput("policy");
-  parser->registerOutput("value");
+  // 18 is the feature size for lz/elf, and the convolution order is channel first.
+  parser->registerInput(input_name.c_str(), DimsCHW(18, 19, 19), UffInputOrder::kNCHW);
+  parser->registerOutput(policy_name.c_str());
+  parser->registerOutput(value_name.c_str());
 
   IBuilder *builder = createInferBuilder(g_logger);
   INetworkDefinition *network = builder->createNetwork();
 
   CHECK(parser->parse(uff_path.c_str(), *network, DataType::kFLOAT));
 
+  /* build engine */
   builder->setMaxBatchSize(FLAGS_max_batch_size);
   builder->setMaxWorkspaceSize(FLAGS_max_workspace_size);
   builder->setHalf2Mode(FLAGS_data_type == "FP16");
@@ -77,10 +87,18 @@ int main(int argc, char *argv[]) {
   ICudaEngine *engine = builder->buildCudaEngine(*network);
   CHECK_NOTNULL(engine);
 
+  /* serialize engine and write to file */
   IHostMemory *serialized_engine = engine->serialize();
 
   std::ofstream output(output_path, std::ios::binary);
   output.write(reinterpret_cast<const char*>(serialized_engine->data()), serialized_engine->size());
+
+  /* break down */
+  builder->destroy();
+  parser->destroy();
+  network->destroy();
+  engine->destroy();
+  serialized_engine->destroy();
 }
 
 #else // GOOGLE_TENSORRT
