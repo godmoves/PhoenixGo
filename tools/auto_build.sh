404aa47a83208db3dba0374421cfb053b89143bf
ManKit Pong <mankit@ManKits-MacBook-Air.local>
2019-01-30 15:26:09 +0800

    update auto build

--- tools/auto_build.sh
+++ tools/auto_build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd "`dirname $0`/.."
+
 bazel build //mcts:mcts_main
 bazel build //mcts:debug_tool
 bazel build //dist:dist_zero_model_server
+bazel build //model:build_tensorrt_model
